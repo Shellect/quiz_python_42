009e05fd117c5cb9e7d7dd3765b3b3be0a9bf06f
Your name <example@example.com>
2025-10-23 19:04:26 +0300

    connect database

--- src/main.py
+++ src/main.py
@@ -1,19 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+
+from src.database import get_db
+from src.models.entities import Question, QuestionOption
 
 app = FastAPI()
 
 @app.get("/question")
-def get_question():
-    return {
-        "question" : "Зимой и летом одним цветом",
-        "answers" : [
-            "Елка",
-            "Медведь",
-            "Небо",
-            "Охотник"
-        ]
-    }
+def get_question(db: Session = Depends(get_db)):
+    try:
+        question = db.query(Question).first()
+        answers = db.query(QuestionOption).filter(QuestionOption.question_id == question.id)
+        answers_texts = [answer.option_text for answer in answers]
 
-@app.get("/health")
-def get_service_health():
-    return list()
+        return {
+            "question": question.question_text,
+            "answers": answers_texts
+        }
+    except:
+        return {"status": "error"}
--- src/models/entities.py
+++ src/models/entities.py
@@ -1,13 +1,25 @@
-from pydantic import BaseModel
-from datetime import datetime
+from sqlalchemy import Column, Integer, Text, String, ForeignKey, Boolean
+from sqlalchemy.orm import relationship
+
+from src.database import Base
 
 
+class Question(Base):
+    __tablename__ = "questions"
 
-class ServiceEntity(BaseModel):
-    name: str
-    url: str
-    check_interval: int
-    timeout: int
-    expected_status: int
-    created_at: datetime
-    is_active: bool = True
+    id = Column(Integer, primary_key=True, index=True)
+    category_id = Column(Integer)
+    question_text = Column(Text, nullable=False)
+
+    options = relationship("QuestionOption", back_populates="question")
+
+
+class QuestionOption(Base):
+    __tablename__ = "question_options"
+
+    id = Column(Integer, primary_key=True, index=True)
+    question_id = Column(Integer, ForeignKey("questions.id", ondelete="CASCADE"))
+    option_text = Column(String(255))
+    is_correct = Column(Boolean, nullable=False, default=False)
+
+    question = relationship("Question", back_populates="options")
